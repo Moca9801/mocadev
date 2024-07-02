80b8396af19d1602eb4a33f6e70fb9c5113a8bff
Moca9801 <aimc9801@gmail.com>
2024-07-01 22:53:16 -0600

    change to scss and define the theme dark and light color

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './layouts/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {   path: '',
+        component: MainComponent,
+        children: [
+
+        ]
+     }
+];
