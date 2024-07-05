018a4263e0a79c0964ea1d2828977b73394d9972
Moca9801 <aimc9801@gmail.com>
2024-07-04 21:31:23 -0600

    skills component completed

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -8,5 +8,34 @@ import { Component } from '@angular/core';
   styleUrl: './skills.component.scss'
 })
 export class SkillsComponent {
+  sections = ['Frontend', 'Backend', 'Design'];
+  currentSectionIndex = 0;
 
+  skills: any = {
+    Frontend: [
+      { name: 'HTML', percentage: 80 },
+      { name: 'CSS', percentage: 85 },
+      { name: 'Bootstrap', percentage: 85 },
+      { name: 'Angular', percentage: 95 },
+      { name: 'Vue', percentage: 65 }
+    ],
+    Backend: [
+      { name: 'Node', percentage: 90 },
+      { name: 'NestJS', percentage: 60 },
+      { name: 'SQL', percentage: 85 }
+    ],
+    Design: [
+      { name: 'Creativity', percentage: 95 },
+      { name: 'Microsoft Publisher', percentage: 100 },
+      { name: 'Figma', percentage: 75 }
+    ]
+  };
+
+  get currentSkills() {
+    return this.skills[this.sections[this.currentSectionIndex]];
+  }
+
+  nextSection() {
+    this.currentSectionIndex = (this.currentSectionIndex + 1) % this.sections.length;
+  }
 }
