2dd63f3a15891503b6f27d04e359ff343f126a11
Moca9801 <aimc9801@gmail.com>
2024-08-02 14:29:51 -0600

    new panel nav for small devices

--- src/app/layouts/main-small-devices/main-small-devices.component.ts
+++ src/app/layouts/main-small-devices/main-small-devices.component.ts
@@ -1,26 +1,27 @@
-import { Component, Renderer2 } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { BioComponent } from '../../components/bio/bio.component';
 import { SkillsComponent } from '../../components/skills/skills.component';
 import { ProjectsComponent } from '../../components/projects/projects.component';
 import { ContactComponent } from '../../components/contact/contact.component';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main-small-devices',
   standalone: true,
-  imports: [HeaderComponent, BioComponent, SkillsComponent, ProjectsComponent, ContactComponent, TranslateModule],
+  imports: [HeaderComponent, BioComponent, SkillsComponent, ProjectsComponent, ContactComponent, TranslateModule, CommonModule],
   templateUrl: './main-small-devices.component.html',
   styleUrl: './main-small-devices.component.scss'
 })
-export class MainSmallDevicesComponent {
+export class MainSmallDevicesComponent{
   currentYear: number = new Date().getFullYear();
   selectedComponent: string = 'Bio';
 
   constructor(private renderer: Renderer2, private translate: TranslateService) {
-    this.translate.setDefaultLang('en');
+    this.translate.setDefaultLang('en');    
   }
-  
+
   ngAfterViewInit() {
     const cursor = this.renderer.selectRootElement('.blob');
 
@@ -30,7 +31,7 @@ export class MainSmallDevicesComponent {
       this.renderer.setStyle(cursor, 'transform', `translate3d(calc(${x}px - 50%), calc(${y}px - 50%), 0)`);
     });
   }
-
+  
   onRadioChange(component: string) {
     this.selectedComponent = component;
   }
